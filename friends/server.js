7f60eb85de1cd674aa7a749d2de944e693f35e3b
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2020-03-26 16:46:57 +0100

    Nauka robocze 26.03.20r godz 16:46

---
+++ friends/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL & Relay Modern is cool!');
+});
+
+
+app.listen(8080, () => console.log('Running server on localhost:8080/graphQL'));
